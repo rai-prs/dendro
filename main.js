bc29412eafbb116337cfd263b48b1556b4a8fa26
EC2 Default User <ec2-user@ip-172-31-23-39.us-west-1.compute.internal>
2022-06-07 08:50:55 +0000

    完成

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+function clickbutton(target) {
+  let result = document.getElementById("result")
+  let operator = document.getElementsByClassName("cul")
+  let target_value = target.innerHTML;
+  let result_value = result.innerHTML.slice(-1)
+  
+  if (target_value == "AC") {
+    result.innerHTML = "0"
+  } else if (target_value == "="){
+    result.innerHTML = eval(result.innerHTML);
+  } else if (result.innerHTML == "0" && (target_value == "+" || target_value == "-" || target_value == "*" || target_value == "/" || target_value == "." || target_value == "0")) {
+      result.innerHTML = "0"
+  } else if (result_value == "+"  && (target_value == "+" || target_value == "-" || target_value =="*" || target_value =="/" || target_value == ".")) {
+                 result.innerHTML == "0"
+  } else if (result_value == "-"  && (target_value == "+" || target_value == "-" || target_value =="*" || target_value =="/" || target_value == ".")) {
+                 result.innerHTML == "0"
+  } else if (result_value == "*"  && (target_value == "+" || target_value == "-" || target_value =="*" || target_value =="/" || target_value == ".")) {
+                 result.innerHTML == "0"
+  } else if (result_value == "/"  && (target_value == "+" || target_value == "-" || target_value =="*" || target_value =="/" || target_value == ".")) {
+                 result.innerHTML == "0"
+  } else if (result_value == "."  && (target_value == "+" || target_value == "-" || target_value =="*" || target_value =="/" || target_value == ".")) {
+                 result.innerHTML == "0"
+  }  else {
+            if (result.innerHTML == "0") {
+              result.innerHTML = target_value
+            } else {
+              result.innerHTML += target_value
+              
+            }
+    }
+}
+
